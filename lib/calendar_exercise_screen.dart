14d0f9d44ad35b8403655b1b114c2afc9b3a0422
PK <piotr.klis0@gmail.com>
2021-07-06 17:49:50 +0200

    added dummy exercise videos & tile

--- lib/calendar_exercise_screen.dart
+++ lib/calendar_exercise_screen.dart
@@ -1,19 +1,71 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:video_player/video_player.dart';
 
-class CalendarExerciseScreen extends StatelessWidget {
+import 'VideoItem.dart';
+
+class CalendarExerciseScreen extends StatefulWidget {
   final int index;
 
   CalendarExerciseScreen({Key? key, required this.index}) : super(key: key);
 
   @override
+  _CalendarExerciseScreenState createState() => _CalendarExerciseScreenState();
+}
+
+class _CalendarExerciseScreenState extends State<CalendarExerciseScreen> {
+  final videoPlayerController = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Calendar'),
+      appBar: AppBar(
+        title: Text('Calendar'),
+      ),
+      //passing in the ListView.builder
+      body: Column(children: [
+        CalendarWidget(),
+        Divider(),
+        Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const ListTile(
+                leading: Icon(Icons.fitness_center),
+                title: Text('I am a title'),
+              ),
+              VideoItem(
+                videoPlayerController: VideoPlayerController.network(
+                    'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4'
+                ),
+                looping: false,
+                autoplay: true,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: const Text(
+                        '#CARDIO',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                  Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: const Text(
+                        '#STRENGTH',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                ],
+              ),
+            ],
+          ),
         ),
-        //passing in the ListView.builder
-        body: CalendarWidget());
+      ]),
+    );
   }
 }
 
@@ -27,7 +79,7 @@ class CalendarWidget extends StatefulWidget {
 class _CalendarWidgetState extends State<CalendarWidget> {
   DateTime _selectedDay = DateTime.now();
   CalendarFormat _calendarFormat = CalendarFormat.week;
-  List<Event> _list = <Event>[];
+  List<bool> _list = <bool>[];
 
   @override
   void initState() {
@@ -36,9 +88,9 @@ class _CalendarWidgetState extends State<CalendarWidget> {
 
   @override
   Widget build(BuildContext context) {
-    _list.add(Event("title"));
+    _list.add(true);
 
-    return TableCalendar<Event>(
+    return TableCalendar<bool>(
       firstDay: DateTime.utc(2010, 10, 16),
       lastDay: DateTime.utc(2030, 3, 14),
       focusedDay: _selectedDay,
@@ -68,11 +120,4 @@ class _CalendarWidgetState extends State<CalendarWidget> {
   }
 }
 
-class Event {
-  final String title;
 
-  const Event(this.title);
-
-  @override
-  String toString() => title;
-}
