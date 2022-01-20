c547a1708d6f9f6d1698dd48eb328b29e7d26fcf
Piotr Klis <p.klis@bsgroup.eu>
2022-01-21 00:24:41 +0100

    events kinda works - they reload every time when date with events is selected

--- lib/app/widget/calendar/calendar_bloc.dart
+++ lib/app/widget/calendar/calendar_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:personal_trainer/app/screen/calendar_exercises/calendar_exercises_bloc.dart';
+import 'package:personal_trainer/domain/model/user_exercise.dart';
 
 import 'calendar_event.dart';
 import 'calendar_provider.dart';
@@ -11,18 +12,18 @@ import 'calendar_state.dart';
 class CalendarBloc extends Bloc<CalendarEvent, CalendarState> {
   final CalendarProvider _calendarExerciseProvider =
       GetIt.I.get<CalendarProvider>();
-  Map<DateTime, bool> events = {};
-  late final StreamSubscription otherBlocSubscription;
-  var exercisesCache = [];
+  Map<DateTime, bool> _events = {};
+  late final StreamSubscription _otherBlocSubscription;
+  List<UserExercise> _exercisesCache = [];
 
   CalendarBloc(
       CalendarState initialState, CalendarExercisesBloc calendarExercisesBloc)
       : super(initialState) {
-    otherBlocSubscription = calendarExercisesBloc.stream.listen((state) {
+    _otherBlocSubscription = calendarExercisesBloc.stream.listen((state) {
       state.whenOrNull(content: (exercises) {
-        if (exercisesCache.isEmpty && exercises.isNotEmpty ||
-            exercisesCache.isNotEmpty && exercises.isEmpty) {
-          exercisesCache = exercises;
+        if (_exercisesCache.isEmpty && exercises.isNotEmpty ||
+            _exercisesCache.isNotEmpty && exercises.isEmpty) {
+          _exercisesCache = List.of(exercises);
           emit(CalendarState.loadEvents());
         }
       });
@@ -32,10 +33,10 @@ class CalendarBloc extends Bloc<CalendarEvent, CalendarState> {
       try {
         var result = await _calendarExerciseProvider.getExerciseMarkerFor(
             dateTime: event.dateTime, clientId: event.clientId);
-        events.addAll({event.dateTime: result});
-        if (events.length == 7) {
-          emitter(CalendarState.eventForDate(events: events));
-          events = {};
+        _events.addAll({event.dateTime: result});
+        if (_events.length == 7) {
+          emitter(CalendarState.eventForDate(events: _events));
+          _events = {};
         }
       } catch (error) {
         emitter(CalendarState.error(error: error.toString()));
@@ -44,12 +45,12 @@ class CalendarBloc extends Bloc<CalendarEvent, CalendarState> {
   }
 
   clearExercisesCache() {
-    exercisesCache = [];
+    _exercisesCache = [];
   }
 
   @override
   Future<void> close() {
-    otherBlocSubscription.cancel();
+    _otherBlocSubscription.cancel();
     return super.close();
   }
 }
